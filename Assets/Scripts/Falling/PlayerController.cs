b3338cb144d572a8f1ad0b128215cb9450827072
BalgroHub <antoniogov1999@gmail.com>
2022-08-23 14:46:30 +0200

    WIP Colisión mortal y retoque de las velocidades

--- Assets/Scripts/Falling/PlayerController.cs
+++ Assets/Scripts/Falling/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
     public float jumpCooldown;
     public float dashCooldown;
     public float maxFallingSpeed;
+    public float mortalSpeed;
     
 
     public float pushForce;
@@ -20,6 +21,8 @@ public class PlayerController : MonoBehaviour
     private float dashTimeRemaining;
 
     private float rockAngularVelocity;
+
+    private Vector2 velocityBeforeCollision;
 
     //Private
     private Rigidbody2D myRigidbody;
@@ -84,7 +87,15 @@ public class PlayerController : MonoBehaviour
     {
         //Limitamos la velocidad de caida
         if (myRigidbody.velocity.y < maxFallingSpeed) myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, maxFallingSpeed);
+
+        //Y guardamos cada frame la velocidad del objeto, para saber que velocidad tenia al colisionar
+        velocityBeforeCollision = myRigidbody.velocity;
     }
+
+
+
+
+    //-----COLISIONES Y TRIGGERS-----
 
     //Si impulsa una roca, le mete velocidad
     private void OnTriggerEnter2D(Collider2D collision)
@@ -92,7 +103,7 @@ public class PlayerController : MonoBehaviour
         //Solo si el objeto es launchable
         if (collision.gameObject.layer == 14 )
         {
-            Debug.Log("Colisionado");
+            //Debug.Log("Colisionado");
             Vector2 rockVelocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
             rockVelocity.y = pushForce;
             collision.gameObject.GetComponent<Rigidbody2D>().velocity = rockVelocity;
@@ -100,6 +111,32 @@ public class PlayerController : MonoBehaviour
             collision.gameObject.GetComponent<Rigidbody2D>().angularVelocity = rockAngularVelocity;
         }
     }
+
+    //Si se choca con un obstaculo
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Layer de los obstaculos
+        if(collision.gameObject.layer == 15)
+        {
+            Debug.Log(velocityBeforeCollision.y);
+            //Si la velocidad es mayor que la soportada, nos morimos
+            if(velocityBeforeCollision.y <= mortalSpeed)
+            {
+                playerDeath();
+            }
+
+        }
+    }
+
+
+    //-----FUNCIONES PROPIAS-----
+
+    private void playerDeath()
+    {
+        Debug.Log("Me he muerto");
+    }
+
+    //-----CORRUTINAS-----
 
     //Empieza el cooldown y lo baja poco a poco hasta que es 0
     private IEnumerator StartJumpCooldown()
@@ -123,7 +160,7 @@ public class PlayerController : MonoBehaviour
         for (dashTimeRemaining = dashCooldown; dashTimeRemaining > 0f; dashTimeRemaining -= 0.1f)
         {
             yield return new WaitForSeconds(0.1f);
-            Debug.Log(dashTimeRemaining);
+            //Debug.Log(dashTimeRemaining);
         }
         dashTimeRemaining = 0f;
     }
